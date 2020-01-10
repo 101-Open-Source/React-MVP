d1ecf4b8917f674658bf75e0af5a65517556c2d1
Seif-Miehiar <Seif.Miehiar@gmail.com>
2020-01-10 18:33:49 +0100

    sending data from react to express test

--- react-mvp/src/App.js
+++ react-mvp/src/App.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React , {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+ constructor(props) {
+   super(props);
+
+   this.state = {
+   input: ""
+  };
+ }
+ 
+ handleClick() {
+  console.log('Click happened');
+}
+onChange(e) {
+  e.preventDefault();
+
+
+}
+  render(){
   return (
     <div className="App">
       <header className="App-header">
@@ -10,17 +27,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <input type='text' placeholder='send me' /><br/>
+       <button onClick={this.handleClick}> Submit</button>
       </header>
     </div>
   );
 }
-
+}
 export default App;
